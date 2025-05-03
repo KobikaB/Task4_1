99e70691b2137f7a916d14f32cd4973aab189dd9
kobika <bkobika06@gmail.com>
2025-05-03 22:06:58 +0530

    updated

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom'
 const Header = () => {
   return (
     
-    <div className='fixed top-0 w-screen bg-gray-200 h-20  px-6 flex items-center justify-between shadow-md ' >
+    <div className='fixed top-0 w-screen bg-gray-200 h-20  px-6 flex items-center justify-between  ' >
       
      <div className='flex m-2  gap-2 flex-shrink-0' >
         <img src="/Image/logo1.png" alt="i11" className='h-15 w-15  ' />
@@ -12,8 +12,10 @@ const Header = () => {
 
       </div>
 
-      <div className='hidden md:flex '>
-    <ul className='flex ml-3 justify-center gap-4 font-semibold  '>
+      <div className='flex justify-end '>
+      <div className="hidden md:flex ">
+
+    <ul className='flex justify-center gap-4   '>
     
     
         <li><Link to='/'>Home</Link></li>
@@ -24,14 +26,15 @@ const Header = () => {
         <li><Link to='/aboutus'>About us</Link></li>
         <li><Link to='/contactus'>Contact us</Link></li> 
         </ul>
+        </div>
         
-       <ul className='flex gap-2 font-semibold'>
+       <ul className='flex gap-2'>
         <li><Link to='/login' className=' rounded-xl m-4 px-3 py-2  hover:bg-gray-200 '>Login</Link></li>
         <li><Link to='/register' className='bg-blue-500 m-4 px-2 py-2 rounded-xl'>Register</Link></li>
         </ul>
 
         </div>
-        
+
    
     </div>
     
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -5,9 +5,9 @@ import image1 from  '../assets/Image/image1.jpeg'
 const Home = () => {
   return (
    <div className=' '>
-    <img src={image1} alt="i1" className='w-screen h-screen bg-cover  object-cover bg-center  '/>
-    <div className="absolute top-1/2 flex  justify-center  ">
-      <h1 className='text-white text-4xl sm:text-5xl md:text-6xl lg:text-7xl font-bold text-center  '>  Where agricultural innovation meets community empowerment</h1>
+    <img src={image1} alt="i1" className='w-screen h-screen bg-cover  object-cover bg-center bg-fixed '/>
+    <div className="absolute top-1/2 ">
+      <h1 className="text-white text-4xl  md:text-5xl lg:text-6xl font-bold text-center "> Where agricultural innovation meets community empowerment</h1>
       </div>
    </div>
   )
